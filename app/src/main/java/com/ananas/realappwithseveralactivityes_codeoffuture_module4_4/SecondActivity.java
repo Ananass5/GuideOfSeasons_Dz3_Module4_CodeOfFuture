7009358f7d34e0aa4e02fb784d8e3e992a7550d5
Ananas <sub.yar@ya.ru>
2023-04-24 19:47:42 +0300

    Новая версия, не то писал. Теперь для RecycleView

--- app/src/main/java/com/ananas/realappwithseveralactivityes_codeoffuture_module4_4/SecondActivity.java
+++ app/src/main/java/com/ananas/realappwithseveralactivityes_codeoffuture_module4_4/SecondActivity.java
@@ -7,47 +7,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
-public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
+public class SecondActivity extends AppCompatActivity {
 
-    private ImageButton spring, summer, autumn, winter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        spring = findViewById(R.id.spring);
-        summer = findViewById(R.id.summer);
-        autumn = findViewById(R.id.autumn);
-        winter = findViewById(R.id.winter);
 
-        spring.setOnClickListener(this);
-        summer.setOnClickListener(this);
-        autumn.setOnClickListener(this);
-        winter.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.spring:
-                Intent intentSpring = new Intent(getApplicationContext(), SpringActivity.class);
-                startActivity(intentSpring);
-                break;
-            case R.id.summer:
-                Intent intentSummer = new Intent(getApplicationContext(), SummerActivity.class);
-                startActivity(intentSummer);
-                break;
-            case R.id.autumn:
-                Intent intentAutumn = new Intent(getApplicationContext(), AutumnActivity.class);
-                startActivity(intentAutumn);
-                break;
-            case R.id.winter:
-                Intent intentWinter = new Intent(getApplicationContext(), WinterActivity.class);
-                startActivity(intentWinter);
-                break;
-            default: break;
 
-        }
-    }
 }
